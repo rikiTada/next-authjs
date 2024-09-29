047563dac9fedcd6042941135632dbea8d6cfa23
rikiTada <riki.tada777@gmail.com>
2024-09-29 21:47:36 +0900

    Refactor auth.ts to add Google provider and session callback

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,19 @@
+import { db } from "@/db";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import type { NextAuthConfig } from "next-auth";
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 export const authConfig = {
-  providers: [GitHub],
+  adapter: DrizzleAdapter(db),
+  providers: [GitHub, Google],
+  callbacks: {
+    async session({ session, user }) {
+      session.user.id = user.id;
+      return session;
+    },
+  },
 } satisfies NextAuthConfig;
 
 export const { handlers, auth, signOut } = NextAuth(authConfig);
