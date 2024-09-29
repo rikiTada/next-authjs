2bdd16f880f1e843f6d2146da0a0bf006ef0335a
rikiTada <riki.tada777@gmail.com>
2024-09-30 08:36:40 +0900

    接続URLの修正

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   schema: "./src/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.POSTGRES_URL!,
+    url: process.env.DATABASE_URL!,
   },
   out: "./drizzle",
 });
--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 
 const pool = new Pool({
-  connectionString: "postgres://user:password@host:port/db",
+  connectionString: process.env.DATABASE_URL,
 });
 
 export const db = drizzle(pool);
