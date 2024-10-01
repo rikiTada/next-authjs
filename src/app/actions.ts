95d5cd6bea8f688651295cea5bdcef55e0ed0104
rikiTada <riki.tada777@gmail.com>
2024-10-02 00:50:04 +0900

    remove unused code

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,11 +1,11 @@
-"use server";
-import { db } from "@/db";
-import { users } from "@/db/schema";
+// "use server";
+// import { db } from "@/db";
+// import { users } from "@/db/schema/auth-schema";
 
-export const insertData = async () => {
-  await db.insert(users).values({
-    name: "hoge",
-    email: "hoge",
-    password: "hoge",
-  });
-};
+// export const insertData = async () => {
+//   await db.insert(users).values({
+//     name: "hoge",
+//     email: "hoge",
+//     password: "hoge",
+//   });
+// };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,18 @@
-"use client";
-
-import { insertData } from "@/app/actions";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       HOME
-      <button
+      <Link href="/me">auth</Link>
+      {/* <button
         className="px-4 py-2 bg-blue-500 text-white rounded-md"
         onClick={async () => {
           await insertData();
         }}
       >
         hoge
-      </button>
+      </button> */}
     </main>
   );
 }
