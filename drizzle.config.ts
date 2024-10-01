e5910bf368cf352673adb0424d20c66c530d72cc
rikiTada <riki.tada777@gmail.com>
2024-10-02 00:23:02 +0900

    Update database schema file path in drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,7 @@ import * as dotenv from "dotenv";
 dotenv.config({ path: ".env.local" });
 
 export default defineConfig({
-  schema: "./src/db/schema.ts",
+  schema: "./src/db/schema/*ts",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
